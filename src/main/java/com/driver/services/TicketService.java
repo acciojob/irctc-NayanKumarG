427c24c0d596966134c2cfe52102fbad6f861921
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-10-04 14:46:23 +0530

    irctc updated

--- src/main/java/com/driver/services/TicketService.java
+++ src/main/java/com/driver/services/TicketService.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TicketService {
