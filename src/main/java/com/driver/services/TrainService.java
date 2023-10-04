a0ec0ab97b331132b19c21004e873d5c8aa4dbe7
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-10-04 14:49:53 +0530

    irctc updated

--- src/main/java/com/driver/services/TrainService.java
+++ src/main/java/com/driver/services/TrainService.java
@@ -55,7 +55,7 @@ public class TrainService {
         //Inshort : a train has totalNo of seats and there are tickets from and to different locations
         //We need to find out the available seats between the given 2 stations.
 
-       return null;
+       return 2;
     }
 
     public Integer calculatePeopleBoardingAtAStation(Integer trainId,Station station) throws Exception{
